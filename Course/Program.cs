d43944e2d40226491984a7856fb749afb6389a34
Djalma <djalma.ant@gmail.com>
2020-05-02 23:52:58 -0300

    Using Static Methods

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Course.Services;
 
 namespace Course
 {
@@ -6,7 +7,13 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = 12;
+            double b = 10;
+
+            double r1 = CalculationService.Max(a, b);
+            double r2 = CalculationService.Sum(a, b);
+            double r3 = CalculationService.Square(a);
+            Console.WriteLine(r1 + " " + r2 + " " + r3);
         }
     }
 }
