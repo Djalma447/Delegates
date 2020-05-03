afa86f7cc85c4484640342355bc0a5e11ad2b09e
Djalma <djalma.ant@gmail.com>
2020-05-03 00:39:09 -0300

    Using Delegate Object

--- Course/Program.cs
+++ Course/Program.cs
@@ -5,15 +5,18 @@ namespace Course
 {
     class Program
     {
+
+        delegate double BinaryNumericOperation(double n1, double n2); // Delegate - Referência para uma Função que recebe dois 'double's' e retorna um número double
+
         static void Main(string[] args)
         {
             double a = 12;
             double b = 10;
 
-            double r1 = CalculationService.Max(a, b);
-            double r2 = CalculationService.Sum(a, b);
-            double r3 = CalculationService.Square(a);
-            Console.WriteLine(r1 + " " + r2 + " " + r3);
+            BinaryNumericOperation op = CalculationService.Sum; // Declaração de um objeto, do Tipo do Delegate criado, usado como referência para o Método Static
+
+            double r1 = op(a, b); // Usando o Método Static através do objeto tipo Delegate declarado
+            Console.WriteLine(r1);
         }
     }
 }
